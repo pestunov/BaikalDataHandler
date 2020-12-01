ca38a0324a8ac012b007a8c8bd65c423d2390167
Dima Pestunov <pest@iao.ru>
2020-12-01 19:53:31 +0700

    Add files via upload

---
+++ test1df.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 17 10:40:45 2020
+
+@author: Phil
+"""
+
+import pandas as pd
+import numpy as np
+
+PATH = r'./data/'
+FILE = 'dec19hourly.csv'
+FULLPATH = ''.join([PATH, FILE])
+print(FULLPATH)
+rowsToSkip = list(range(2,1000))
+df = pd.read_csv(FULLPATH,
+                 index_col='DateTime',
+                 header=0,
+                 skiprows=list(range(2,1000)),
+                 na_values=['--'],
+                 parse_dates=['DateTime'],
+                 decimal=',',
+                 usecols=['DateTime','vCO2air','PICARRO_CO2'],
+                 nrows=1000,
+                 )
+
+df = df[df.index.notna()]
+
+df['time'] = df.index.time
+
+dr = pd.date_range(start=df.index.min().date(),
+                   end=df.index.max().date(),
+                   freq='H')[:-1].to_pydatetime()
+
